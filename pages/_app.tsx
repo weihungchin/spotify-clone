b91810888905412aa6ebcc123bb6123fe9d4b8d0
WeiHung <wei_hung.chin@daimler.com>
2022-04-12 20:13:21 +0800

    Add playerLayout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "reset-css";
+import PlayerLayout from "./../components/playerLayout";
 
 // copy from material design theme
 const theme = extendTheme({
@@ -33,7 +34,9 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />;
+      <PlayerLayout>
+        <Component {...pageProps} />;
+      </PlayerLayout>
     </ChakraProvider>
   );
 }
